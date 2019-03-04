5a226eb14c4f137cec2fb661c6c47f8b0b51b35e
Fabio Bombardi <shadowsheep@gmail.com>
2019-03-05 00:07:24 +0100

    Minimal stub project

--- app/src/main/java/com/example/fcmtest/MainActivity.java
+++ app/src/main/java/com/example/fcmtest/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.fcmtest;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView fcmTokenTextView = findViewById(R.id.fcmToken);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+                        fcmTokenTextView.setText(token);
+                        Log.d(TAG, "FCMToken: " + token);
+                    }
+                });
     }
 }
